62ef85e5fae90d13a65f19ae6c3e568e5ba8d886
MSDOMAIN1\skodag <sourabh.kodag@morningstar.com>
2022-05-08 17:24:07 +0530

    latest

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require('express')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const serveStatic = require('serve-static')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 
 
